f6461b05a5265d807058bfa7f4bcb00c177c5e2e
haha246 <b09902109@ntu.edu.tw>
2020-10-06 11:50:40 +0800

    second commit

--- main.js
+++ main.js
@@ -5,10 +5,44 @@ var button = document.getElementById("cal-button")
 input.value = ''
 color.value = '#b0b0b0'
 
+var a = new Array(cells.length);
+
+for(var j = 0; j < cells.length; j++){
+	cells[j].onclick = function(){
+		for(var i = 0; i < cells.length; i++){
+			cells[i].setAttribute("id","")
+			cells[i].style.backgroundColor = ""
+		}
+		this.setAttribute("id", "chosen")
+		var k = document.getElementById("chosen")
+		k.style.backgroundColor = "#000"
+		}
+}
 
 
 
+button.onclick = function(){
+	var c = document.getElementById("chosen")
+	var comment = input.value
+	input.value = ''
+	var e = document.createElement("p")
+	c.appendChild(e)
+	e.innerHTML = comment
+	c.style.backgroundColor = "#AF3"
+	c.setAttribute("id", "")
+}
 
+input.addEventListener('keyup', event => {if(event.keyCode === 13 && event.target.value !== ''){
+	var c = document.getElementById("chosen")
+	var comment = input.value
+	input.value = ''
+	var e = document.createElement("p")
+	c.appendChild(e)
+	e.innerHTML = comment
+	c.style.backgroundColor = "#AF3"
+	c.setAttribute("id", "")
+}
+})
 
 
 
